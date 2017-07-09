072166d71f72b6ce72d7cce4f27cb88cb2c0d3a2
PiotrBerebecki <petebere@gmail.com>
2017-07-10 00:10:35 +0100

    Remove comments from fruit controller

--- src/controllers/fruit.js
+++ src/controllers/fruit.js
@@ -1,8 +1,6 @@
 const request = require('request');
 
 exports.getFruit = (req, res) => {
-  // res.setHeader('Content-Type', 'text/html');
-
   request(
     'https://jsonplaceholder.typicode.com/photos',
     (err, response, body) => {
@@ -10,12 +8,6 @@ exports.getFruit = (req, res) => {
         activePage: { fruit: true },
         fruit: JSON.parse(body).slice(0, 10),
       });
-      // res.end();
     }
   );
-
-  // res.write('fruit', {
-  //   activePage: { fruit: true },
-  //   fruit: [],
-  // });
 };
